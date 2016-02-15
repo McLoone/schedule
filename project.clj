fedac93af5ec1cdaa0268769b8a9cdf35756d857
P-A Söderqvist <pa.soderqvist@gmail.com>
2016-02-15 12:47:40 +0100

    Adding initial liberator setup

--- project.clj
+++ project.clj
@@ -1,2 +1,7 @@
 (defproject schedule "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler api/handler}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [liberator "0.14.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-core "1.4.0"]])
